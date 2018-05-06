aea422404765b0211ce6c185753666f3a34281f5
Bart Janssens <bart@bartjanssens.org>
2018-05-06 20:32:15 +0200

    Update REQUIRE and libcxxwrap-julia version

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using Compat
 using BinDeps
 import BinDeps.generate_steps
 
-const JLCXX_VERSION = v"0.2.0"
+const JLCXX_VERSION = v"0.2.1"
 const use_master = false
 
 JLCXX_LIBDIR=get(ENV, "JLCXX_LIBDIR", "")
