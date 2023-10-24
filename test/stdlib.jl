d0db82494063cc6e9c6859e770f83a8ad19b389a
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-23 22:20:30 -0500

    Refactor StdVector tests

--- test/stdlib.jl
+++ test/stdlib.jl
@@ -141,34 +141,45 @@ end
   end
 end
 
-stvec = StdVector(Int32[1,2,3])
-@test all(stvec .== [1,2,3])
-push!(stvec,1)
-@test all(stvec .== [1,2,3,1])
-resize!(stvec,2)
-@test all(stvec .== [1,2])
-append!(stvec,Int32[2,1])
-@test all(stvec .== [1,2,2,1])
-empty!(stvec)
-@test isempty(stvec)
+@testset "StdVector" begin
+  @testset "constructors" begin
+    @test StdVector([1,2,3]) == [1,2,3]
+    @test StdVector([1.0, 2.0, 3.0]) == [1,2,3]
+    @test StdVector([true, false, true]) == [true, false, true]
+  end
 
-@test all(StdVector([1,2,3]) .== [1,2,3])
-@test all(StdVector([1.0,2.0,3.0]) .== [1,2,3])
-@test all(StdVector([true, false, true]) .== [true, false, true])
-bvec = StdVector([true, false, true])
-append!(bvec, [true])
-@test all(bvec .== [true, false, true, true])
+  @testset "mutating with integer" begin
+    stvec = StdVector(Int32[1,2,3])
+    @test stvec == [1,2,3]
+    push!(stvec,1)
+    @test stvec == [1,2,3,1]
+    resize!(stvec, 2)
+    @test stvec == [1,2]
+    append!(stvec, Int32[2,1])
+    @test stvec == [1,2,2,1]
+    empty!(stvec)
+    @test isempty(stvec)
+  end
 
-cxxstrings = StdString["one", "two", "three"]
-svec = StdVector(CxxRef.(cxxstrings))
-@test all(svec .== ["one", "two", "three"])
-push!(svec, StdString("four"))
-@test all(svec .== ["one", "two", "three", "four"])
-cxxappstrings = StdString["five", "six"]
-append!(svec, CxxRef.(cxxappstrings))
-@test all(svec .== ["one", "two", "three", "four", "five", "six"])
-empty!(svec)
-@test isempty(svec)
+  @testset "mutating with bool" begin
+    bvec = StdVector([true, false, true])
+    append!(bvec, [true])
+    @test bvec == [true, false, true, true]
+  end
+
+  @testset "mutating with StdString" begin
+    cxxstrings = StdString["one", "two", "three"]
+    svec = StdVector(CxxRef.(cxxstrings))
+    @test svec == ["one", "two", "three"]
+    push!(svec, StdString("four"))
+    @test svec == ["one", "two", "three", "four"]
+    cxxappstrings = StdString["five", "six"]
+    append!(svec, CxxRef.(cxxappstrings))
+    @test svec == ["one", "two", "three", "four", "five", "six"]
+    empty!(svec)
+    @test isempty(svec)
+  end
+end
 
 stvec = StdVector(Int32[1,2,3])
 for vref in (CxxRef(stvec), CxxPtr(stvec))
