7d0733259df353e7d39b75692b91bef906a78c9f
Praneeth Jain <praneethjain005@gmail.com>
2024-06-16 13:20:06 +0530

    feat: StdPriorityQueue (#441)
    * feat: StdPriorityQueue
    
    * test: StdPriorityQueue

--- src/StdLib.jl
+++ src/StdLib.jl
@@ -230,6 +230,18 @@ for StdMultisetType in (StdMultiset, StdUnorderedMultiset)
   Base.count(x, v::StdMultisetType) = multiset_count(v, x)
 end
 
+Base.size(v::StdPriorityQueue) = (Int(cppsize(v)),)
+Base.length(v::StdPriorityQueue) = Int(cppsize(v))
+Base.isempty(v::StdPriorityQueue) = pq_isempty(v)
+Base.first(v::StdPriorityQueue) = isempty(v) ? nothing : pq_top(v)
+Base.push!(v::StdPriorityQueue, x) = (pq_push!(v, x); v)
+function Base.pop!(v::StdPriorityQueue)
+  isempty(v) && throw(ArgumentError("Cannot pop from an empty priority queue"))
+  val = pq_top(v)
+  pq_pop!(v)
+  return val
+end
+
 function Base.fill!(v::T, x) where T <: Union{StdVector, StdValArray, StdDeque}
   StdFill(v, x)
   return v
