a82df04799951dc92240b126d69fefc946c182a7
Abdo Eid <abdoeid.eg@ieee.org>
2024-06-20 19:03:02 +0300

    undo the renaming parameter StdDeque from `d` to `v`

--- src/StdLib.jl
+++ src/StdLib.jl
@@ -132,15 +132,15 @@ function StdDeque() where {T}
   return StdDeque{T}()
 end
 Base.IndexStyle(::Type{<:StdDeque}) = IndexLinear()
-Base.size(v::StdDeque) = (Int(cppsize(v)),)
-Base.getindex(v::StdDeque, i::Int) = cxxgetindex(v,i)[]
-Base.setindex!(v::StdDeque{T}, val, i::Int) where {T} = cxxsetindex(v, convert(T,val), i)
-Base.push!(v::StdDeque, x) = push_back(v, x)
-Base.pushfirst!(v::StdDeque, x) = push_front(v, x)
-Base.pop!(v::StdDeque) = pop_back(v)
-Base.popfirst!(v::StdDeque) = pop_front(v)
-Base.resize!(v::StdDeque, n::Integer) = resize(v, n)
-Base.empty!(v::StdDeque) = clear(v)
+Base.size(d::StdDeque) = (Int(cppsize(v)),)
+Base.getindex(d::StdDeque, i::Int) = cxxgetindex(v,i)[]
+Base.setindex!(d::StdDeque{T}, val, i::Int) where {T} = cxxsetindex(v, convert(T,val), i)
+Base.push!(d::StdDeque, x) = push_back(v, x)
+Base.pushfirst!(d::StdDeque, x) = push_front(v, x)
+Base.pop!(d::StdDeque) = pop_back(v)
+Base.popfirst!(d::StdDeque) = pop_front(v)
+Base.resize!(d::StdDeque, n::Integer) = resize(v, n)
+Base.empty!(d::StdDeque) = clear(v)
 
 Base.:(==)(a::StdDequeIterator, b::StdDequeIterator) = iterator_is_equal(a,b)
 function _iteration_tuple(d::StdDeque, state::StdDequeIterator)
