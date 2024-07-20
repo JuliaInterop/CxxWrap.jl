863d52fe3a4097afbcbad957c35339e3d233b75f
PraneethJain <praneethjain005@gmail.com>
2024-07-20 11:06:40 +0530

    feat: StdList iterator

--- src/StdLib/StdList.jl
+++ src/StdLib/StdList.jl
@@ -10,3 +10,9 @@ Base.push!(v::StdList, x) = (list_push_back!(v, x); v)
 Base.pushfirst!(v::StdList, x) = (list_push_front!(v, x); v)
 Base.pop!(v::StdList) = (list_pop_back!(v); v)
 Base.popfirst!(v::StdList) = (list_pop_front!(v); v)
+
+
+Base.:(==)(a::StdListIterator, b::StdListIterator) = iterator_is_equal(a, b)
+_list_iteration_tuple(v::StdList, state::StdListIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
+Base.iterate(v::StdList) = _list_iteration_tuple(v, iteratorbegin(v))
+Base.iterate(v::StdList, state::StdListIterator) = (state != iteratorend(v)) ? _list_iteration_tuple(v, iterator_next(state)) : nothing
