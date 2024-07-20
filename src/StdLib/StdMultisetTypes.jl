31cbd44207e8e9948fcae08d67811ff105d0a93e
PraneethJain <praneethjain005@gmail.com>
2024-07-20 21:33:28 +0530

    feat: multiset and unordered multiset iterators

--- src/StdLib/StdMultisetTypes.jl
+++ src/StdLib/StdMultisetTypes.jl
@@ -10,3 +10,13 @@ for StdMultisetType in (StdMultiset, StdUnorderedMultiset)
     Base.delete!(v::StdMultisetType, x) = (multiset_delete!(v, x); v)
     Base.count(x, v::StdMultisetType) = multiset_count(v, x)
 end
+
+Base.:(==)(a::StdMultisetIterator, b::StdMultisetIterator) = iterator_is_equal(a, b)
+_multiset_iteration_tuple(v::StdMultiset, state::StdMultisetIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
+Base.iterate(v::StdMultiset) = _multiset_iteration_tuple(v, iteratorbegin(v))
+Base.iterate(v::StdMultiset, state::StdMultisetIterator) = (state != iteratorend(v)) ? _multiset_iteration_tuple(v, iterator_next(state)) : nothing
+
+Base.:(==)(a::StdUnorderedMultisetIterator, b::StdUnorderedMultisetIterator) = iterator_is_equal(a, b)
+_unordered_multiset_iteration_tuple(v::StdUnorderedMultiset, state::StdUnorderedMultisetIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
+Base.iterate(v::StdUnorderedMultiset) = _unordered_multiset_iteration_tuple(v, iteratorbegin(v))
+Base.iterate(v::StdUnorderedMultiset, state::StdUnorderedMultisetIterator) = (state != iteratorend(v)) ? _unordered_multiset_iteration_tuple(v, iterator_next(state)) : nothing
