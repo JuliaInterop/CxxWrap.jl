9effd70202cf6a83c2a5b9c5f492f05063615a9b
PraneethJain <praneethjain005@gmail.com>
2024-07-20 20:37:49 +0530

    feat: set iterator

--- src/StdLib/StdSetTypes.jl
+++ src/StdLib/StdSetTypes.jl
@@ -9,3 +9,8 @@ for StdSetType in (StdSet, StdUnorderedSet)
     Base.in(x, v::StdSetType) = set_in(v, x)
     Base.delete!(v::StdSetType, x) = (set_delete!(v, x); v)
 end
+
+Base.:(==)(a::StdSetIterator, b::StdSetIterator) = iterator_is_equal(a, b)
+_set_iteration_tuple(v::StdSet, state::StdSetIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
+Base.iterate(v::StdSet) = _set_iteration_tuple(v, iteratorbegin(v))
+Base.iterate(v::StdSet, state::StdSetIterator) = (state != iteratorend(v)) ? _set_iteration_tuple(v, iterator_next(state)) : nothing
