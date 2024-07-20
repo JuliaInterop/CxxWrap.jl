577e315bcec1a6229c4f4bf440c305abac205f7a
PraneethJain <praneethjain005@gmail.com>
2024-07-20 20:59:00 +0530

    feat: unordered set iterator

--- src/StdLib/StdSetTypes.jl
+++ src/StdLib/StdSetTypes.jl
@@ -14,3 +14,8 @@ Base.:(==)(a::StdSetIterator, b::StdSetIterator) = iterator_is_equal(a, b)
 _set_iteration_tuple(v::StdSet, state::StdSetIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
 Base.iterate(v::StdSet) = _set_iteration_tuple(v, iteratorbegin(v))
 Base.iterate(v::StdSet, state::StdSetIterator) = (state != iteratorend(v)) ? _set_iteration_tuple(v, iterator_next(state)) : nothing
+
+Base.:(==)(a::StdUnorderedSetIterator, b::StdUnorderedSetIterator) = iterator_is_equal(a, b)
+_unordered_set_iteration_tuple(v::StdUnorderedSet, state::StdUnorderedSetIterator) = (state == iteratorend(v)) ? nothing : (iterator_value(state), state)
+Base.iterate(v::StdUnorderedSet) = _unordered_set_iteration_tuple(v, iteratorbegin(v))
+Base.iterate(v::StdUnorderedSet, state::StdUnorderedSetIterator) = (state != iteratorend(v)) ? _unordered_set_iteration_tuple(v, iterator_next(state)) : nothing
